303a1310fc3fe35800cb909113dd1b0e37ee77bc
DarbyLee-Stack <68654827+DarbyLee-Stack@users.noreply.github.com>
2020-07-27 14:41:57 -0400

    Add files via upload

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Darby Lee-Stack on 7/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    
+    var description = " "
+    var important = false 
+}
